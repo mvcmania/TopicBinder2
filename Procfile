3273e81c73ab8682404daba4f166f3b6fbe5d608
ecipil <erkncpl@gmail.com>
2017-04-08 22:52:07 +0300

    merge

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node app.js
+web: node /examples/messenger.js
--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -6,9 +6,56 @@ var Pool = require('../../models/pool');
 router.get('/',function(req,res){
      Pool.find({},function(err,pools){
         res.render('dashboard',{ pools:pools });
-     });   
-    
+     });
+
+});
+router.post('/',function(req,res,next){
+    var busboy = new Busboy({headers : req.headers});
+    busboy.on('file',function(fieldname,file,filename,encoding,mimetype){
+        var all_rows='';
+        file.on('data',function(data){
+          all_rows += data;
+        });
+        file.on('end',function(data){
+          console.log('Finished with read data : file name :'+filename);
+          csv_parse(all_rows,res);
+        });
+    });
+    busboy.on('finish', function() {
+      console.log('Done parsing form!');
+    });
+
+    req.pipe(busboy);
+    res.end();
+    //res.render('dashboard');
 });
 
+function csv_parse(records,res){
+  //Split with new line
+  var arr = records.split("\n");
+  var pool_array=[];
+  arr.map(function(val){
+    var splitted = val.split(" ");
+    var obj = {
+      "document_id" : splitted[2],
+      "topic_id" : splitted[0],
+      "score" : parseInt(splitted[3]),
+      "isrelated" : false,
+     };
+     pool_array.push(obj);
+  });
+  if(pool_array.length > 0){
+      Pool.collection.insertMany(pool_array,function(err,docs){
+        if(err){
+            console.log("Error occured..on bulk pool saving...",err);
+            res.status(500).send('error occured..on bulk pool saving...'+ err);
+        }else{
+            console.info('here is the saving result : %o ',docs);
+            res.status(200).redirect('/');
+        }
+      //  res.end();
+    });
+  }
+}
 
 module.exports = router;
